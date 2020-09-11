685ee91eb359eb8805499c33774776deaf6233c7
longgt <longgt@aureole-it.vn>
2020-09-11 19:38:55 +0700

    feat: intial commit

---
+++ src/main/java/fish/payara/examples/payaramicro/spring/boot/domain/Person.java
@@ -0,0 +1,67 @@
+/*
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ Copyright (c) 2015 C2B2 Consulting Limited. All rights reserved.
+ The contents of this file are subject to the terms of the Common Development
+ and Distribution License("CDDL") (collectively, the "License").  You
+ may not use this file except in compliance with the License.  You can
+ obtain a copy of the License at
+ https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ or packager/legal/LICENSE.txt.  See the License for the specific
+ language governing permissions and limitations under the License.
+ When distributing the software, include this License Header Notice in each
+ file and include the License file at packager/legal/LICENSE.txt.
+ */
+package fish.payara.examples.payaramicro.spring.boot.domain;
+
+/**
+ * Created by mertcaliskan
+ */
+public class Person {
+
+    private int id;
+    private String name;
+    private String lastName;
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, String lastName, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
